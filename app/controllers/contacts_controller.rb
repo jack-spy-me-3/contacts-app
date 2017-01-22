93b3e449b1e2044faa3da7e51150c7da5d4cc363
Samantha Morco <samanthamorco@gmail.com>
2017-01-22 13:58:47 -0500

    new contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,4 +9,14 @@ class ContactsController < ApplicationController
     @contacts = Contact.all
     render "all_contacts_info.html.erb"
   end
+
+  def new_contact_form_method
+    render "new_contact_form_page.html.erb"
+  end
+
+  def new_contact_result_method
+    @contact = Contact.new(first_name: params[:input_first_name], last_name: params[:input_last_name], email: params[:input_email], phone_number: params[:input_phone])
+    @contact.save
+    render "new_contact_result_page.html.erb"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/first_contact", to: "contacts#first_contact_method"
   get "/all_contacts", to: "contacts#all_contacts_method"
+
+  get "/new_contact_form", to: "contacts#new_contact_form_method"
+  post "/new_contact_result", to: "contacts#new_contact_result_method"
 end
