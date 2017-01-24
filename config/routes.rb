d1795db326fc9d770032a0a1a1776ee3ada3b66a
Samantha Morco <samanthamorco@gmail.com>
2017-01-23 20:17:36 -0500

    add restful routes to contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/first_contact", to: "contacts#first_contact_method"
-  get "/all_contacts", to: "contacts#all_contacts_method"
-
-  get "/new_contact_form", to: "contacts#new_contact_form_method"
-  post "/new_contact_result", to: "contacts#new_contact_result_method"
+  get "/contacts", to: "contacts#index"
+  get "/contacts/new", to: "contacts#new"
+  post "/contacts", to: "contacts#create"
+  get "/contacts/:id", to: "contacts#show"
+  get "/contacts/:id/edit", to: "contacts#edit"
+  patch "/contacts/:id", to: "contacts#update"
+  delete "/contacts/:id", to: "contacts#destroy"
 end
