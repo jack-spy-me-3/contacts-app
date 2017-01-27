2ef3bff80f17a4e35bf7d72003b6bdc3f614e411
Samantha Morco <samanthamorco@gmail.com>
2017-01-26 19:24:01 -0500

    add seed data and seed dump gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Contact.create!([
+  {first_name: "John", last_name: "Messina", email: "john@messina.com", phone_number: "555-123-1234", middle_name: "J", bio: nil},
+  {first_name: "Mike", last_name: "Yatco", email: "mike@yatco.com", phone_number: "555-343-3434", middle_name: "J", bio: nil},
+  {first_name: "Andy", last_name: "Zhao", email: "andy@zhao.com", phone_number: "333-333-3333", middle_name: "J", bio: nil},
+  {first_name: "Peter", last_name: "Strong", email: "peterstrong@actualize.co", phone_number: "555-555-1234", middle_name: "J", bio: nil},
+  {first_name: "John", last_name: "Messina", email: "john@messina.com", phone_number: "555-123-1234", middle_name: "J", bio: nil},
+  {first_name: "Mike", last_name: "Yatco", email: "mike@yatco.com", phone_number: "555-343-3434", middle_name: "J", bio: nil},
+  {first_name: "Andrew", last_name: "Zhao", email: "andy@zhao.com", phone_number: "333-333-3333", middle_name: "J", bio: nil},
+  {first_name: "Peter", last_name: "Strong", email: "peterstrong@actualize.co", phone_number: "555-555-1233", middle_name: "J", bio: nil},
+  {first_name: "Peter", last_name: "Rostov", email: "jonathan@coll.com", phone_number: "555-343-1111", middle_name: "J", bio: nil},
+  {first_name: "John", last_name: "sdfsdfds", email: "sdfsfsd@gmail.com", phone_number: "34243242342342342342343234234", middle_name: "J", bio: nil},
+  {first_name: "John", last_name: "Jacob", email: "Jingleheimer Schmidt", phone_number: "123123", middle_name: nil, bio: nil},
+  {first_name: "John", last_name: "Jingleheimer Schmidt", email: "j@gmail.com", phone_number: "32432423423", middle_name: "Jacob", bio: nil},
+  {first_name: "fsdfds", last_name: "sfsdf", email: "sdfsd", phone_number: "dsfdsf", middle_name: "", bio: nil},
+  {first_name: "Bob", last_name: nil, email: nil, phone_number: nil, middle_name: "J", bio: nil},
+  {first_name: "Joe", last_name: nil, email: nil, phone_number: nil, middle_name: "J", bio: nil},
+  {first_name: "Susan", last_name: nil, email: nil, phone_number: nil, middle_name: "J", bio: nil}
+])
